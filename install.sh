acfbff03501e1d521ba6a5ce408ea6a2c383a4fb
Ryo Shen <shenlw66@gmail.com>
2022-07-24 20:02:43 +0800

    feat: update mac pathrc

--- install.sh
+++ install.sh
@@ -70,6 +70,7 @@ link_file_list()
     link_file  ~/.my_config/home/.vimrc ~/.vimrc
     link_file  ~/.my_config/home/.zshrc ~/.zshrc
     link_file  ~/.my_config/home/.pathrc ~/.pathrc
+    link_file  ~/.my_config/home/.pathrc.mac ~/.pathrc.mac
     link_file  ~/.my_config/home/.aliasrc ~/.aliasrc
     link_file  ~/.my_config/home/.condarc ~/.condarc
     link_file  ~/.my_config/home/.npmrc ~/.npmrc
